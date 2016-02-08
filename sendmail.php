f9e577937e5dc28b14c57a4ee8c6cfb9791b0d5c
croossin <croossin94@gmail.com>
2016-02-08 14:30:40 -0600

    Added back in the other emails

--- sendmail.php
+++ sendmail.php
@@ -5,10 +5,10 @@
 	jwagman@wisc.edu	
 */
 
-$subject = 	'[BadgerLoop Contact]' . $_POST['subject'];
+$subject = 	'[BadgerLoop Contact] ' . $_POST['subject'];
 
-  // mail( "jwagman@wisc.edu", $_POST['subject'], $_POST['message'], "From: " . $_POST['email']);
-  // mail( "mgoldberg4@wisc.edu", $_POST['subject'], $_POST['message'], "From: " . $_POST['email']);
-  // mail( "sbsmith5@wisc.edu", $_POST['subject'], $_POST['message'], "From: " . $_POST['email']);
+  mail( "jwagman@wisc.edu", $subject, $_POST['message'], "From: " . $_POST['email']);
+  mail( "mgoldberg4@wisc.edu", $subject, $_POST['message'], "From: " . $_POST['email']);
+  mail( "sbsmith5@wisc.edu", $subject, $_POST['message'], "From: " . $_POST['email']);
   mail( "croossin94@gmail.com", $subject, $_POST['message'], "From: " . $_POST['email']);
 ?>
