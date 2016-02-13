05d3dfd44a7770e6dfab49168b9e9a7691cdd1d6
croossin <croossin94@gmail.com>
2016-02-13 16:57:43 -0600

    Changed email list

--- sendmail.php
+++ sendmail.php
@@ -8,7 +8,6 @@
 $subject = 	'[BadgerLoop Contact] ' . $_POST['subject'];
 
   mail( "jwagman@wisc.edu", $subject, $_POST['message'], "From: " . $_POST['email']);
-  mail( "mgoldberg4@wisc.edu", $subject, $_POST['message'], "From: " . $_POST['email']);
-  mail( "sbsmith5@wisc.edu", $subject, $_POST['message'], "From: " . $_POST['email']);
+  mail( "kemcnally@wisc.edu", $subject, $_POST['message'], "From: " . $_POST['email']);
   mail( "croossin94@gmail.com", $subject, $_POST['message'], "From: " . $_POST['email']);
 ?>
